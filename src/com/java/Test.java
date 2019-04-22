e893995ebe70dd4fe180108c9d092579555ae246
GD3 <GD3@DESKTOP-SL3939K>
2019-04-22 17:39:59 +0900

    수정파일

--- src/com/java/Test.java
+++ src/com/java/Test.java
@@ -2,8 +2,9 @@ package com.java;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("");
 
 	}
 
